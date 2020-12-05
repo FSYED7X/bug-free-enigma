a6cac8558d5bf9469da99760039291636e21dde6
Farhan Syed <fsyed7x@gmail.com>
2020-12-05 12:56:02 +0530

    done

---
+++ script.js
@@ -0,0 +1,79 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementsByTagName('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit',(event) =>{
+    
+    event.preventDefault();
+    checkInputs();
+});
+
+function checkInputs(){
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    const password2Value = password2.value;
+
+    if(usernameValue ===""){
+       
+        setErrorfor(username,"Username cannot be blank");
+    }
+    else{
+        //add success
+        setSuccessfor(username);
+    }
+
+   if(emailValue ===''){
+   
+    setErrorfor(email,"email cannot be blank");
+    }
+   
+    else{
+        //add success
+        setSuccessfor(email);
+    }
+    if(passwordValue ===''){
+        
+        setErrorfor(password,"password cannot be blank");
+    }
+    else{
+        //add success
+        setSuccessfor(password);
+    }
+    if (password2Value ===''){
+        
+        setErrorfor(password2," confirm password");
+    }
+    else if(passwordValue ==password2Value)
+    {
+        setErrorfor(password2 ,"password does not match");
+    }
+    else{
+        //add success
+        setSuccessfor(password2);
+    }
+
+    //success
+
+}
+
+function setErrorfor(input,message)
+{
+    const formitem = input.parentNode;
+    // console.log(input.parentNode,formitem)
+    const small= formitem.querySelector('small');
+
+    
+    small.innerHTML = message;
+
+    formitem.className = "form-item success";
+}
+
+function setSuccessfor(input) 
+{ 
+    const formitem = input.parentNode;
+    console.log(formitem)
+    formitem.className = "form-item error";
+}
